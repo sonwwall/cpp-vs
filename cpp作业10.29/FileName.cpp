b229b6d9a17a217d4923e4089589e6e9cf849461
sonwwall <2233like@gmail.com>
2024-11-11 10:00:14 +0800

    11/11修改：使用循环和计算

--- cpp作业10.29/FileName.cpp
+++ cpp作业10.29/FileName.cpp
@@ -1,18 +1,48 @@
 #include<iostream>
-#include<string>
+//#include<string>
 using namespace std;
+//int main()
+//{
+//	string old;
+//	getline(cin, old);
+//	int length = size(old);
+//	string result;
+//	for (int i = 0; i < length; i++)
+//	{
+//		result+=( old[length - i]);
+//	}
+//	cout << result;
+//	cout << endl;
+//	cout << length;
+
+//}//纯使用字符串
 int main()
 {
-	string old;
-	getline(cin, old);
-	int length = size(old);
-	string result;
-	for (int i = 0; i < length; i++)
+	int num;
+	cin >> num;
+	int j = 1;
+	int n = 0;
+	while (1)
 	{
-		result+=( old[length - i]);
+		if (num / j == 0)
+		{
+			break;
+		}
+		j *= 10;
+		n++;
+	}
+	j /= 10;
+	cout <<"位数：" << n << endl;
+	int result=0;
+	int multi = 1;
+	for (int i = 0; i < n; i++)
+	{
+		result += (num / j) * multi;
+		num -= (num / j) * j;
+
+		multi *= 10;
+		j /= 10;
 	}
 	cout << result;
-	cout << endl;
-	cout << length;
 
-}//纯使用字符串
+}
